3fb3d2cfb22e0c9186378569c53dd4375786497d
Andrea Zichittella <zichittella@emagine.it>
2020-02-06 18:31:35 +0100

    Add CopyToClipboard

--- 23_Final_Project/massive-color-project/src/ColorBox.js
+++ 23_Final_Project/massive-color-project/src/ColorBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 import './ColorBox.css'
 
 export default class ColorBox extends Component {
@@ -6,22 +8,24 @@ export default class ColorBox extends Component {
         super(props)
 
         this.state = {
-                 
+
         }
     }
 
     render() {
-        const {name, background} = this.props;
+        const { name, background } = this.props;
         return (
-            <div style={{background}} className="ColorBox">
-                <div className="copy-container">
-                    <div className="box-content">
-                        <span>{name}</span>
+            <CopyToClipboard text={ background }>
+                <div style={{ background }} className="ColorBox">
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <span>{name}</span>
+                        </div>
+                        <button className="copy-button">Copy</button>
                     </div>
-                    <button className="copy-button">Copy</button>
+                    <span className="see-more">More</span>
                 </div>
-                <span className="see-more">More</span>
-            </div>
+            </CopyToClipboard>
         )
     }
 }
