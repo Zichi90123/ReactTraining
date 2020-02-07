8970233b03a91d17dddeb40d73b236c2906585e6
Andrea Zichittella <zichittella@emagine.it>
2020-02-07 15:25:38 +0100

    Add Select and MAterial UI

--- 23_Final_Project/massive-color-project/src/Navbar.js
+++ 23_Final_Project/massive-color-project/src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Select, MenuItem } from "@material-ui/core";
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
@@ -8,12 +9,19 @@ export default class Navbar extends Component {
         super(props)
 
         this.state = {
-
+            format: 'hex'
         }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({format: e.target.value});
+        this.props.handleChange(e.target.value)
     }
 
     render() {
         const { level, changeLevel } = this.props
+        const { format } = this.state
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -30,6 +38,13 @@ export default class Navbar extends Component {
                             onAfterChange={changeLevel}
                         />
                     </div>
+                </div>
+                <div className="select-container">
+                    <Select value={format} onChange={this.handleChange}>
+                        <MenuItem value="hex">HEX - #fffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+                    </Select>
                 </div>
             </header>
         )
