71c50447a6c06ae8680d2e680637ddf5f3615daa
Andrea Zichittella <zichittella@emagine.it>
2020-02-11 12:46:11 +0100

    Move out Styles

--- 23_Final_Project/massive-color-project/src/Palette.js
+++ 23_Final_Project/massive-color-project/src/Palette.js
@@ -2,20 +2,11 @@ import React, { Component } from 'react'
 import ColorBox from './ColorBox'
 import {withStyles} from "@material-ui/styles";
 
-import './Palette.css'
+import styles from './styles/PaletteStyles'
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter'
 
-const styles = {
-    Palette: {
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    colors: {
-        height: '90%',
-    }
-}
+
 
 class Palette extends Component {
 
