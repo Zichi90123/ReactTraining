0ee910e4dc082c048ab95354d350b5ea69444cd4
Andrea Zichittella <zichittella@emagine.it>
2020-02-11 09:59:39 +0100

    Add Navbar and Footer to SingleColorPalette

--- 23_Final_Project/massive-color-project/src/Navbar.js
+++ 23_Final_Project/massive-color-project/src/Navbar.js
@@ -28,25 +28,27 @@ export default class Navbar extends Component {
     }
 
     render() {
-        const { level, changeLevel } = this.props
+        const { level, changeLevel, showingAllColors } = this.props
         const { format } = this.state
         return (
             <header className="Navbar">
                 <div className="logo">
                     <Link to="/">reactcolorpicker</Link>
                 </div>
-                <div className="slider-container">
-                    <span>Level: {level}</span>
-                    <div className="slider">
-                        <Slider
-                            defaultValue={level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={changeLevel}
-                        />
+                {showingAllColors && (
+                    <div className="slider-container">
+                        <span>Level: {level}</span>
+                        <div className="slider">
+                            <Slider
+                                defaultValue={level}
+                                min={100}
+                                max={900}
+                                step={100}
+                                onAfterChange={changeLevel}
+                            />
+                        </div>
                     </div>
-                </div>
+                )}
                 <div className="select-container">
                     <Select value={format} onChange={this.handleFormatChange}>
                         <MenuItem value="hex">HEX - #fffff</MenuItem>
@@ -62,7 +64,7 @@ export default class Navbar extends Component {
                     ContentProps={{
                         "aria-describedby": "message-id"
                     }}
-                    onClose={ this.closeSnackbar }
+                    onClose={this.closeSnackbar}
                     action={[
                         <IconButton
                             onClick={this.closeSnackbar}
--- 23_Final_Project/massive-color-project/src/SingleColorPalette.js
+++ 23_Final_Project/massive-color-project/src/SingleColorPalette.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import PaletteFooter from './PaletteFooter';
 
 export default class SingleColorPalette extends Component {
     constructor(props) {
         super(props)
 
+        this.state ={format: "hex"}
         this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+
+        this.changeFormat = this.changeFormat.bind(this);
     }
 
     gatherShades(palette, colorToFilterBy) {
@@ -19,16 +24,24 @@ export default class SingleColorPalette extends Component {
         return shades.slice(1);
     }
 
+    changeFormat(val) {
+        this.setState({ format: val })
+    }
+
+
     render() {
+        const {format} = this.state
+        const {paletteName, emoji} = this.props.palette
         const colorBoxes = this._shades.map(color => (
-            <ColorBox key={color.name} name={color.name} background={color.hex} showLink={false} />
+            <ColorBox key={color.name} name={color.name} background={color[format]} showLink={false} />
         ))
         return (
             <div className="Palette">
-                <h1>Single Color Palette</h1>
+                <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
+                <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         )
     }
