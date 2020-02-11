644c611afc571db18a2f02321333cd47a327eead
Andrea Zichittella <zichittella@emagine.it>
2020-02-11 14:54:54 +0100

    Add NewPaletteForm and Link

--- 23_Final_Project/massive-color-project/src/App.js
+++ 23_Final_Project/massive-color-project/src/App.js
@@ -5,6 +5,7 @@ import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 
 class App extends Component {
   findPalette(id) {
@@ -16,6 +17,21 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route
+          exact
+          path="/palette/new"
+          render={() => <NewPaletteForm />}
+        />
+        <Route
+          exact
+          path="/palette/:paletteId/:colorId"
+          render={(routeProps) => (
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+            />
+          )}
+        />
         <Route
           exact
           path="/"
@@ -32,16 +48,6 @@ class App extends Component {
           render={(routeProps) => (
             <Palette
               palette={generatePalette(this.findPalette(routeProps.match.params.id))}
-            />
-          )}
-        />
-        <Route
-          exact
-          path="/palette/:paletteId/:colorId"
-          render={(routeProps) => (
-            <SingleColorPalette
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
             />
           )}
         />
