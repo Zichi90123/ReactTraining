2e75b429f4328a72e1dbe3b7d6c197e77128dad5
Andrea Zichittella <zichittella@emagine.it>
2020-02-14 15:09:30 +0100

    Add bg svg to PaletteList

--- 23_Final_Project/massive-color-project/src/styles/PaletteListStyles.js
+++ 23_Final_Project/massive-color-project/src/styles/PaletteListStyles.js
@@ -1,12 +1,16 @@
 import sizes from './sizes'
+import bg from './bg.svg'
 
 export default {
     root: {
-        backgroundColor: "blue",
         height: "100vh",
         display: "flex",
         alignItems: "flex-start",
-        justifyContent: "center"
+        justifyContent: "center",
+        /* background by SVGBackgrounds.com */
+        backgroundColor: "#394bad",
+        backgroundImage: `url(${bg})`,
+        overflow: "scroll"
     },
     container: {
         width: "50%",
@@ -16,7 +20,13 @@ export default {
         flexWrap: "wrap",
         [sizes.down("xl")]: {
             width: "80%"
+        },
+        [sizes.down("xs")]: {
+            width: "70%"
         }
+    },
+    heading: {
+        fontSize: "2rem"
     },
     nav: {
         display: "flex",
