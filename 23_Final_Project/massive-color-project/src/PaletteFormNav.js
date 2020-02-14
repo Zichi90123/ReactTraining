32348092b1687650bc384cd87e95f2c952d064fa
Andrea Zichittella <zichittella@emagine.it>
2020-02-14 12:09:40 +0100

    Add ResponsiveHelper and style ColorBox

--- 23_Final_Project/massive-color-project/src/PaletteFormNav.js
+++ 23_Final_Project/massive-color-project/src/PaletteFormNav.js
@@ -6,7 +6,6 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos'
 import IconButton from "@material-ui/core/IconButton";
 import Button from '@material-ui/core/Button';
