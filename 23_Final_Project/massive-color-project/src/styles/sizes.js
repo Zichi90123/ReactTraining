ff6d1741cfa91946d64070d91e26df2a46541e54
Andrea Zichittella <zichittella@emagine.it>
2020-02-14 14:51:40 +0100

    Make PaletteList Responsive

--- 23_Final_Project/massive-color-project/src/styles/sizes.js
+++ 23_Final_Project/massive-color-project/src/styles/sizes.js
@@ -7,7 +7,8 @@ export default {
             xs: "573.98px",
             sm: "767.98px",
             md: "991.98px",
-            lg: "1199.98px"
+            lg: "1199.98px",
+            xl: "1600px"
         }
         return `@media (max-width: ${sizes[size]})`
     }
