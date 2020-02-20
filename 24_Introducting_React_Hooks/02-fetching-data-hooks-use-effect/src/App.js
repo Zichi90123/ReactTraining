c2f89e870a75a439d3fb027dd0f6cd36c285513a
Andrea Zichittella <zichittella@emagine.it>
2020-02-20 15:10:26 +0100

    Introduction on Hooks

---
+++ 24_Introducting_React_Hooks/02-fetching-data-hooks-use-effect/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SWMovies from './SWMovies';
+
+function App() {
+  return (
+    <div className="App">
+      <SWMovies />
+    </div>
+  );
+}
+
+export default App;
